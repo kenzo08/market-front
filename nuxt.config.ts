5e71b614e5a3acab569aa2a4f738f60acd101bcf
Bekhrooz Abdoovakhidov <behruzabduvahidov@gmail.com>
2025-06-17 23:07:06 +0500

    some configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import process from 'node:process';
+
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, user-scalable=no',
+      title: 'Market',
+      htmlAttrs: {
+        lang: 'ru',
+      },
+      link: [],
+      script: [],
+    },
+  },
+  nitro: {
+    compressPublicAssets: true,
+  },
+  router: {
+    options: {
+      scrollBehaviorType: 'smooth',
+    },
+  },  runtimeConfig: {
+    public: {
+      API_BASE_URL: process.env.NUXT_API_BASE,
+
+    },
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true }
 })
